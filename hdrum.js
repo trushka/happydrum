71fe3172d035cc6578c7dfd9288f98234ab57b9b
trushka <trueshko@gmail.com>
2023-10-22 15:29:54 +0300

    svg cleanup and glow effect

---
+++ hdrum.js
@@ -0,0 +1,10 @@
+const $=jQuery;
+
+const container=$('#hdrum-vidget');
+const petals = $('[data-petal]').each((i, petal)=>{
+	const audio = $('<audio>').prop({
+		//src: 'notes'
+	})
+});
+
+console.log(import.meta)
